80b8149a71011d83f89a75f502092a13b958665c
Michael Mills <rizz1337@gmail.com>
2017-01-24 00:13:36 -0800

    Change inputs to string values only

--- client/App.js
+++ client/App.js
@@ -1,3 +1,21 @@
 console.log('Hello World!');
 
-angular.module('rocket', []);
+angular.module('rocketApp', [])
+
+  .controller('rocketController', function ($scope) {
+
+    $scope.myRockets = [];
+
+    $scope.rocket.fullMass = 0;
+    $scope.rocket.emptyMass = 0;
+    $scope.rocket.engineISP = 0;
+
+    $scope.rocket.deltav = null;
+
+    $scope.calculate = function() {
+      if ($scope.rocket.fullMass > 0 && $scope.rocket.emptyMass > 0 && $scope.rocket.engineISP > 0) {
+        $scope.rocket.deltav = $scope.rocket.engineISP * 9.8 * Math.log($scope.rocket.fullMass / $scope.rocket.emptyMass);
+      }
+    };
+
+});
