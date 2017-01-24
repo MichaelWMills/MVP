9506be3098f77a96033487e32a8e5caded51dc62
Michael Mills <rizz1337@gmail.com>
2017-01-24 12:28:01 -0800

    Integrate database, refine view

--- client/App.js
+++ client/App.js
@@ -1,21 +1,57 @@
-console.log('Hello World!');
-
 angular.module('rocketApp', [])
 
-  .controller('rocketController', function ($scope) {
+  .controller('rocketController', function ($scope, $http) {
 
     $scope.myRockets = [];
 
+    $scope.rocket.name = '';
     $scope.rocket.fullMass = 0;
-    $scope.rocket.emptyMass = 0;
+    $scope.rocket.dryMass = 0;
     $scope.rocket.engineISP = 0;
 
-    $scope.rocket.deltav = null;
+    $scope.rocket.deltav = 0;
+
+    $scope.getAllRockets = function() {
+      console.log('getAllRockets was called');
+      $http({
+        method:'GET',
+        url:'/rocketList'
+      })
+      .then(function(rockets) {
+        console.log('Rockets retrieved successfully');
+        rockets.data.forEach(function(rocket) {
+          $scope.myRockets.push(rocket);
+        });
+      });
+    };
+
+    $scope.addRocket = function(rocket) {
+      console.log('addRocket was called');
+      $http({
+        method:'POST',
+        url:'/rocketList',
+        data: rocket
+      })
+      .then(function() {
+        console.log('Added rocket successfully');
+        $scope.getAllRockets();
+      });
+    };
 
     $scope.calculate = function() {
-      if ($scope.rocket.fullMass > 0 && $scope.rocket.emptyMass > 0 && $scope.rocket.engineISP > 0) {
-        $scope.rocket.deltav = $scope.rocket.engineISP * 9.8 * Math.log($scope.rocket.fullMass / $scope.rocket.emptyMass);
+      if ($scope.rocket.fullMass > 0 && $scope.rocket.dryMass > 0 && $scope.rocket.engineISP > 0) {
+        $scope.rocket.deltav = $scope.rocket.engineISP * 9.8 * Math.log($scope.rocket.fullMass / $scope.rocket.dryMass);
+        $scope.rocket.deltav = $scope.rocket.deltav.toFixed(2);
       }
+      var myRocket = {
+        'model': $scope.rocket.name,
+        'engineisp': $scope.rocket.engineISP,
+        'fullmass': $scope.rocket.fullMass,
+        'drymass': $scope.rocket.dryMass,
+        'deltav': $scope.rocket.deltav
+      };
+
+      $scope.addRocket(myRocket);
     };
 
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,61 @@
 var express = require('express');
-var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+var app = express();
 var port = process.env.PORT || 8000;
 
+
+mongoose.connect('mongodb://localhost/db');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+  console.log('Successfully connected to DB');
+});
+
+var rocketSchema = mongoose.Schema({
+  model: String,
+  emptyMass: Number,
+  fullMass: Number,
+  engineisp: Number,
+  deltav: Number
+});
+
+var Rocket = mongoose.model('Rocket', rocketSchema);
+
 app.use(express.static('./client'));
+app.use(bodyParser());
 
 app.listen(port, function() {
   console.log('Server connected on ' + port);
+});
+
+app.post('/rocketList', function(req, res) {
+  var rocket = new Rocket({
+    model: req.body.model,
+    emptyMass: req.body.emptyMass,
+    fullMass: req.body.fullMass,
+    engineisp: req.body.engine,
+    deltav: req.body.deltav
+  });
+
+  rocket.save(function(err) {
+    if(err){
+      throw err;
+    } else {
+      console.log('rocket saved successfully');
+    }
+  });
+});
+
+app.get('/rocketList', function(req, res) {
+  Rocket.find({}, function(err, rockets) {
+    if (err){
+      throw err;
+    } else {
+      res.send(rockets);
+    }
+  });
 });
